edac0f09152d0a62fd0ffe9966196f68a5894cb1
TheGodlyLuzer <luca.schaefer1594@gmail.com>
2021-06-16 11:35:44 +0200

    Text change

--- src/App/pages/HUD/Hotel/components/HotelBuy/HotelBuy.js
+++ src/App/pages/HUD/Hotel/components/HotelBuy/HotelBuy.js
@@ -5,7 +5,7 @@ import './HotelBuy.scss';
 const HotelBuy = ({price, event}) => {
     return <div className='hotel-buy'>
         <div className='hotel-buy__price'>
-            Preis<br/>{price}
+            Preis<br/>{price} / Stunde
         </div>
         <div className='hotel-buy__button' onClick={event}>Mieten</div>
     </div>
